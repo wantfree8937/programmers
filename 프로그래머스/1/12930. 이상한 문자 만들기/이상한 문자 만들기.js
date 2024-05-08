acba4ada2abf1f7fab3454be05f95c2256c0a541
wantfree8937 <101966192+wantfree8937@users.noreply.github.com>
2024-05-08 09:35:46 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.28 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,6 @@
+function solution(s) {
+    return s
+        .split(' ')
+        .map(word => word.split('').map((char, index) => index % 2 === 0 ? char.toUpperCase() : char.toLowerCase()).join(''))
+        .join(' ');
+}
