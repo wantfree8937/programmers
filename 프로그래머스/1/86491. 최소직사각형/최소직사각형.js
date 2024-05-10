f35df1ad2b7b6b79331964386eaae3e54a06bedc
wantfree8937 <101966192+wantfree8937@users.noreply.github.com>
2024-05-10 09:35:42 +0900

    [level 1] Title: 최소직사각형, Time: 2.54 ms, Memory: 38.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,12 @@
+function solution(sizes) {
+    let wmax = 0;
+    let hmax = 0;
+    for (let item of sizes) {
+        if (item[0] < item[1]) {
+            item.reverse();
+        }
+        wmax = wmax < item[0] ? item[0] : wmax;
+        hmax = hmax < item[1] ? item[1] : hmax;
+    }
+    return wmax * hmax
+}
