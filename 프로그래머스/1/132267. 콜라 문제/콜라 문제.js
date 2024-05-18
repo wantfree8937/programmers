a270bc6a0c712b557fe81d4d9ec2e50fdaa9d1ec
wantfree8937 <101966192+wantfree8937@users.noreply.github.com>
2024-05-18 20:50:33 +0900

    [level 1] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,18 +1,9 @@
 function solution(empty, coca, n) {
-  let remain = 0;
   let count = 0;
   while (n >= empty) {
-    if (n % empty == 0) {
-      n /= empty;
-      n *= coca;
-      count += n;
-    } else {
-      remain = n % empty;
-      n = Math.floor(n / empty);
-      n *= coca;
-      count += n;
-      n += remain;
-    }
+    let newCoca = Math.floor(n / empty) * coca;
+    count += newCoca;
+    n = newCoca + (n % empty);
   }
   return count;
 }
