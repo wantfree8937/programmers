77a355f49883efca590d04a8528ec1050b797ba3
wantfree8937 <101966192+wantfree8937@users.noreply.github.com>
2024-05-21 09:39:15 +0900

    [level 1] Title: 카드 뭉치, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,9 @@
+function solution(cards1, cards2, goal) {
+  let result = "Yes";
+  for (let i = 0; i < goal.length; i++) {
+    if (goal[i] === cards1[0]) cards1.shift();
+    else if (goal[i] === cards2[0]) cards2.shift();
+    else result = "No";
+  }
+  return result;
+}
