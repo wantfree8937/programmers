287868f839c889bc7148f7c6e0b21da88a69f7e7
wantfree8937 <101966192+wantfree8937@users.noreply.github.com>
2024-05-22 10:18:13 +0900

    [level 1] Title: 과일 장수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,8 @@
+function solution(k, m, score) {
+  let result = 0;
+  score.sort((a, b) => b - a);
+  for(let i = m; i <= score.length; i += m){
+    result += score[i - 1] * m;
+  }
+    return result;
+}
