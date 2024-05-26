5c51f7b835162e11780354eee44347022786d8bf
wantfree8937 <101966192+wantfree8937@users.noreply.github.com>
2024-05-27 08:54:58 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -0,0 +1,8 @@
+function solution(lottos, win_nums) {
+  let rate = [6, 6, 5, 4, 3, 2, 1];
+
+  let lowCount = win_nums.filter(num => lottos.includes(num)).length;
+  let highCount = lottos.filter(zero => zero === 0).length;
+
+  return [rate[lowCount + highCount], highCount === 6 ? 6 : rate[lowCount]];
+}
